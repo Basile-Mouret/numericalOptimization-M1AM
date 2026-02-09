292d75b468a6d7b9cb1a239a617bfa3ee1a7a5e9
Basile <145753507+Basile-Mouret@users.noreply.github.com>
2026-02-09 10:44:44 +0100

    tp2

--- tp2/algorithms.py
+++ tp2/algorithms.py
@@ -63,3 +63,31 @@ def GD_ls(f, grad_f, x_init, tau0, iterMax, prec, rho, c):
     return x,x_tab
 
 
+def GD_sec_order_exact(f, grad_f, hessian_f, x_init, iterMax, prec):
+
+    epsilon = prec*np.linalg.norm(grad_f(x_init))
+
+    x = np.copy(x_init)
+    x_tab = np.copy(x_init)
+
+
+    print("------------------------------------\n GD with constant step size\n------------------------------------\nSTART")
+    t_s =  timeit.default_timer()
+
+    for k in range(iterMax):
+
+        g = grad_f(x)
+        A = hessian_f(x)
+        tau = (g.T@g)/(g.T@A@g) 
+
+        x = x - tau*g
+
+        x_tab = np.vstack((x_tab,x))
+
+        if np.linalg.norm(g) < epsilon:
+            break
+
+    t_e =  timeit.default_timer()
+    print("FINISHED -- {:d} iterations -- {:.6f}s -- final value: {:f} -- final gradient norm: {:f} \n\n".format(k,t_e-t_s,f(x),np.linalg.norm(grad_f(x))))
+    
+    return x,x_tab
